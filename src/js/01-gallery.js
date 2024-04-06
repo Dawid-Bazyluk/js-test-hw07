492970867abfee83b43056eab990741e0b7e0466
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-04-06 03:25:42 +0200

    start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector("gallery");
+const galleryCreator = galleryItems.map(
+  (image) => `<div class="gallery__item">
+  <a class="gallery__link" href=${image.original}
+    <img
+      class="gallery__image"
+      src=${image.preview}
+      data-source=${image.original}
+      alt=${image.description}
+    />
+  </a>
+</div>
+`
+).join("");
+
+gallery.insertAdjacentHTML("beforebegin", galleryCreator);
